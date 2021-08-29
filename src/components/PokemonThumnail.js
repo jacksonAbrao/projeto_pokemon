46c391d6d49fda7b2639efe30bb0b4359835edbd
Jackson Abrão <jackson.comp1681@hotmail.com>
2021-08-29 15:27:33 -0300

    primeira parte do projeto concluido

---
+++ src/components/PokemonThumnail.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const PokemonThumnail = ({ id, name, image, type }) => {
+  const style = `thumb-container ${type}`;
+  return (
+    <div className={style}>
+      <div className="number">
+        <small>nº 0{id}</small>
+      </div>
+      <img src={image} alt={name} />
+      <div className="detail-wrapper">
+        <h3>{name}</h3>
+        <small>Type: {type}</small>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonThumnail;
