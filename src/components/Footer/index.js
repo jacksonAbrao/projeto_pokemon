e1be310ba0f94974b27e4fe932d7516e43b6f4d0
Jackson Abrão <jackson.comp1681@hotmail.com>
2021-08-30 18:06:54 -0300

    alterei o tamanho do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,10 +5,10 @@ function Footer() {
   return (
     <footer className="mt-20 grid justify-items-center content-center">
       <div className="flex flex-col md:flex-row items-center">
-        <h1 className="text-gray-600 font-bold text-2xl md:text-6xl text-center ">
+        <h1 className="text-gray-600 font-bold text-2xl md:text-3xl text-center ">
           Developed by
         </h1>
-        <h1 className="text-gray-600 md:ml-4 font-extrabold text-2xl md:text-6xl text-center ">
+        <h1 className="text-gray-600 md:ml-4 font-extrabold text-2xl md:text-3xl text-center ">
           Jackson Abrão
         </h1>
       </div>
